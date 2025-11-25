d7fec49e1d9105e9c55ccaa71ceef6c2f0a97ec7
MariaAise <mashaaise@gmail.com>
2025-11-25 15:12:12 +1100

    Add site and base properties to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress(),]
+  site: 'https://MariaAise.github.io',
+  base: '/play-astro',
+  integrations: [
+    tailwind(),
+    compress(),
+  ],
 });
